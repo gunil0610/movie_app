16096d4042158a8c109813727093bea732f29a0a
gunil na <gunil.na1@gmail.com>
2020-06-22 21:40:41 +0200

    Updates

--- precache-manifest.03b4b10d07ff6c18aa9d5cb883299c62.js
+++ precache-manifest.03b4b10d07ff6c18aa9d5cb883299c62.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ed7a971c474376552e38d6515b6809a9",
+    "revision": "eb0eb887d0c828f670ba51bf3efac4b2",
     "url": "/movie_app/index.html"
   },
   {
-    "revision": "61a8c7a61918a40d7718",
-    "url": "/movie_app/static/css/main.4f7dfffa.chunk.css"
+    "revision": "3241fc24223a2896af61",
+    "url": "/movie_app/static/css/main.40e168a0.chunk.css"
   },
   {
     "revision": "c0071c60e1a78cbbff2e",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "61a8c7a61918a40d7718",
+    "revision": "3241fc24223a2896af61",
     "url": "/movie_app/static/js/main.a840680b.chunk.js"
   },
   {
