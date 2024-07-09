6465f1c8eb06b92492e7cb9ebc33fff795378d62
gunil0610 <gunil.na1@gmail.com>
2024-07-09 14:48:50 +0900

    fix: navigation add search param

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,6 +5,7 @@ function Navigation() {
     <div className="fixed top-[50px] left-[10px] flex flex-col gap-5 rounded-md bg-white py-2.5 px-5 shadow-md">
       <Link
         to="/"
+        search={{ page: 0, limit: 20 }}
         className="text-center text-sm uppercase text-[#0008fc] decoration-0 [&.active]:font-bold"
       >
         Home
